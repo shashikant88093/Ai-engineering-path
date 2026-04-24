7921cab5d867b56ca74c4ad3a2200a8a2c1680ff
Shashikant <42858165+shashikant88093@users.noreply.github.com>
2026-04-24 23:09:59 +0530

    sdded chatbot logic

--- Intro-to-ai-engineering/Ai-backend/index.js
+++ Intro-to-ai-engineering/Ai-backend/index.js
@@ -5,13 +5,19 @@ import OpenAI from "openai";
 checkEnvironment()
 
 
+async function start(){
+console.log(process.env,".env file")
 const openai = new OpenAI({
     apiKey: process.env.AI_KEY,
-    // baseURL: process.env.AI_URL,
-    dangerouslyAllowBrowser: true
+    baseURL: process.env.AI_URL,
+    // dangerouslyAllowBrowser: true
 })
 
-const propmt = "Suggest some gifts for someone who loves hiphop music"
+const inputText = document.getElementById("inputText")
+const viewData = document.getElementById("viewData")
+const apiCall = document.getElementById("apiCall")
+
+const propmt = inputText.trim()
 
 try {
     const response = await openai.chat.completions.create({
@@ -21,10 +27,13 @@ try {
                 'role': "user",
                 content: propmt
             }
-        ]
+        ],
+        max_completion_tokens:256
     })
 
-    console.log(response, "response")
+    console.log(response, "response")                                                                                                                                                                                   
 } catch (err) {
     console.log(err, "error")
+}
+
 }
